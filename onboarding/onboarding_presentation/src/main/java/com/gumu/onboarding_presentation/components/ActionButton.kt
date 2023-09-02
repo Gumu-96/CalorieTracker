797bcff291d4c45def0cfa27617ba32ec7030f8e
Gustavo Müller <gumu96.dev@gmail.com>
2023-09-02 15:51:25 -0600

    :lipstick: Add onboarding welcome screen

---
+++ onboarding/onboarding_presentation/src/main/java/com/gumu/onboarding_presentation/components/ActionButton.kt
@@ -0,0 +1,32 @@
+package com.gumu.onboarding_presentation.components
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gumu.core_ui.theme.CalorieTrackerTheme
+
+@Composable
+fun ActionButton(
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = isEnabled
+    ) {
+        Text(text = text)
+    }
+}
+
+@Preview
+@Composable
+fun ActionButtonPreview() {
+    CalorieTrackerTheme {
+        ActionButton(text = "Do Something") {}
+    }
+}
---
+++ onboarding/onboarding_presentation/src/main/java/com/gumu/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,54 @@
+package com.gumu.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.gumu.core_ui.R
+import com.gumu.core_ui.theme.CalorieTrackerTheme
+import com.gumu.core_ui.theme.LocalSpacing
+import com.gumu.onboarding_presentation.components.ActionButton
+
+@Composable
+fun WelcomeScreen() {
+    val spacing = LocalSpacing.current
+    Surface {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(spacing.spaceMedium)
+        ) {
+            Text(
+                text = stringResource(id = R.string.welcome_text),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            ActionButton(
+                text = stringResource(id = R.string.next),
+                onClick = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WelcomeScreenPreview() {
+    CalorieTrackerTheme {
+        WelcomeScreen()
+    }
+}
