279c736f4bb04fc28de20dbb4cb190349c58cd0a
Gustavo Müller <gumu96.dev@gmail.com>
2023-09-03 11:06:49 -0600

    :construction: Initial hilt setup

--- app/src/main/java/com/gumu/calorietracker/MainActivity.kt
+++ app/src/main/java/com/gumu/calorietracker/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.gumu.calorietracker.navigation.CalorieTrackerNavigation
 import com.gumu.core_ui.theme.CalorieTrackerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
