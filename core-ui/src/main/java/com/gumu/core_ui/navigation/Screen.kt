5955a1b44cb76aaeffdcb0d9047d03d3d0bc1c17
Gustavo Müller <gumu96.dev@gmail.com>
2023-09-10 11:10:03 -0600

    :recycle: Add fun to append params to routes

--- core-ui/src/main/java/com/gumu/core_ui/navigation/Screen.kt
+++ core-ui/src/main/java/com/gumu/core_ui/navigation/Screen.kt
@@ -1,6 +1,9 @@
 package com.gumu.core_ui.navigation
 
 sealed class Screen(val route: String) {
+    private val baseRoute: String =
+        route.substring(0, route.indexOf('/').takeIf { it > -1 } ?: route.length)
+
     // Onboarding
     object Welcome : Screen("welcome_screen")
     object Age : Screen("age_screen")
@@ -13,5 +16,21 @@ sealed class Screen(val route: String) {
 
     // Tracker
     object TrackerOverview : Screen("tracker_overview_screen")
-    object Search : Screen("search_screen")
+    object Search : Screen("search_screen/{$MEAL_TYPE_PARAM}/{$DAY_OF_MONTH_PARAM}/{$MONTH_PARAM}/{$YEAR_PARAM}")
+
+    fun routeWithArgs(vararg args: String): String {
+        return buildString {
+            append(baseRoute)
+            args.forEach {
+                append("/$it")
+            }
+        }
+    }
+
+    companion object {
+        const val MEAL_TYPE_PARAM = "meal_type"
+        const val DAY_OF_MONTH_PARAM = "day_of_month"
+        const val MONTH_PARAM = "month"
+        const val YEAR_PARAM = "year"
+    }
 }
